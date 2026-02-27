24748cd78406864e4eb0159d0d934e93327ed776
ITSfitrah <alpitrah125@gmail.com>
2026-02-27 22:26:30 +0700

    feat : rute publik

---
+++ resources/views/public/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>KSR PMI ITN Malang</title>
+</head>
+<body>
+    <h1>Selamat Datang di Website Resmi KSR PMI ITN Malang</h1>
+    <p>Halaman ini akan dikerjakan oleh tim publik.</p>
+</body>
+</html>

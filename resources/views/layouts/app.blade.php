469357e20becd32410d0fd239222b3b52b7bc76e
ITSfitrah <alpitrah125@gmail.com>
2026-02-28 12:53:42 +0700

    feat : update tampilan dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,17 +15,21 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+        <div class="min-h-screen relative bg-cover bg-center bg-fixed bg-no-repeat" style="background-image: url('{{ asset('images/dashback.jpg') }}');">
+            
+            <div class="absolute inset-0 bg-black/60 backdrop-blur-sm"></div>
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
+            <div class="relative z-10">
+
+                @include('layouts.navigation')
+
+                @if (isset($header))
+                    <header class="bg-white shadow">
+                        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                            {{ $header }}
+                        </div>
+                    </header>
+                @endif
 
             <!-- Page Content -->
             <main>
